e88bc7a7d1d63b9f02f0ad2c0a543fe5e1b13a06
ryoto <ryouto1999.11@gmail.com>
2022-11-26 00:02:00 +0900

    first commit

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ window.onload = function () {
     $(".loader").delay(2000).fadeOut();
 };
 $(function () {
-    console.log("aaa");
-
-
+    console.log("js起動確認");
+    $(".hamburger").click(
+        function(){
+            $(this).toggleClass("active");
+        }
+    )
 
 
 
